f5a9b323177b132b92b27c45c77847688cdce152
小宇不是鱼 <3551371733@qq.com>
2025-12-22 18:42:45 +0800

    Add files via upload

---
+++ src/main/java/com/xiaoyu/custom_sapling/CustomSapling.java
@@ -0,0 +1,50 @@
+package com.xiaoyu.custom_sapling;
+
+import net.minecraft.world.level.material.*;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.item.*;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.*;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+
+@Mod(CustomSapling.MOD_ID)
+public class CustomSapling {
+    public static final String MOD_ID = "custom_sapling";
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+    
+    public static final RegistryObject<Block> CUSTOM_SAPLING = BLOCKS.register("custom_sapling", 
+        () -> new CustomSaplingBlock(
+            BlockBehaviour.Properties.of()
+                .mapColor(MapColor.PLANT)
+                .noCollission()
+                .randomTicks()
+                .instabreak()
+                .sound(SoundType.GRASS)
+                .pushReaction(PushReaction.DESTROY)
+        )
+    );
+    
+    public static final RegistryObject<Item> CUSTOM_SAPLING_ITEM = ITEMS.register("custom_sapling", 
+        () -> new BlockItem(CUSTOM_SAPLING.get(), new Item.Properties()));
+    
+    @SuppressWarnings("removal")
+    public CustomSapling() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ITEMS.register(modEventBus);
+        BLOCKS.register(modEventBus);
+        modEventBus.register(this);
+    }
+    
+    @SubscribeEvent
+    public void buildContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+            event.accept(CUSTOM_SAPLING_ITEM.get());
+        }
+    }
+}
---
+++ src/main/java/com/xiaoyu/custom_sapling/CustomSaplingBlock.java
@@ -0,0 +1,109 @@
+package com.xiaoyu.custom_sapling;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.level.*;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.*;
+import net.minecraft.world.level.block.state.properties.*;
+import net.minecraft.world.level.levelgen.feature.*;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
+
+import java.util.Optional;
+
+public class CustomSaplingBlock extends BushBlock implements BonemealableBlock {
+    public static final IntegerProperty STAGE = BlockStateProperties.STAGE;
+    
+    public CustomSaplingBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(STAGE, Integer.valueOf(0)));
+    }
+    
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(STAGE);
+    }
+
+    @Override
+    public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state, boolean isClient) {
+        return true;
+    }
+
+    @Override
+    public boolean isBonemealSuccess(Level level, RandomSource random, BlockPos pos, BlockState state) {
+        return true;
+    }
+    
+    @Override
+    public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
+        this.growTree(level, pos, state, random);
+    }
+    
+    @Override
+    protected boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos) {
+        return true;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (state.getValue(STAGE) == 0) {
+            level.setBlock(pos, state.setValue(STAGE, Integer.valueOf(1)), 4);
+        } else {
+            this.growTree(level, pos, state, random);
+        }
+
+        super.randomTick(state, level, pos, random);
+    }
+
+    public void growTree(ServerLevel level, BlockPos pos, BlockState state, RandomSource random) {
+        BlockPos belowPos = pos.below();
+        BlockState belowState = level.getBlockState(belowPos);
+        BlockPos belowBelowPos = belowPos.below();
+        BlockState belowBelowState = level.getBlockState(belowBelowPos);
+        
+        BlockState trunkState;
+        if (!belowBelowState.isAir()) {
+            trunkState = belowBelowState;
+        } else {
+            trunkState = belowState;
+        }
+
+        BlockState leavesState = belowState;
+
+        TreeConfiguration config = new TreeConfiguration.TreeConfigurationBuilder(
+            BlockStateProvider.simple(trunkState), 
+            new StraightTrunkPlacer(4, 2, 0),
+            BlockStateProvider.simple(leavesState),
+            new BlobFoliagePlacer(
+                ConstantInt.of(2), 
+                ConstantInt.of(0), 
+                3
+            ),
+            new TwoLayersFeatureSize(1, 0, 1)
+        ).dirt(BlockStateProvider.simple(belowState)).ignoreVines().build();
+
+        level.setBlock(pos, level.getFluidState(pos).createLegacyBlock(), 4);
+
+        boolean success = Feature.TREE.place(
+            new FeaturePlaceContext<>(
+                Optional.empty(),
+                level,
+                level.getChunkSource().getGenerator(),
+                random,
+                pos,
+                config
+            )
+        );
+
+        if (!success) {
+            level.setBlock(pos, state, 4);
+        }
+    }
+}
